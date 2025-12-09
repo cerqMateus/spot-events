55a605dbaf20919cff444cc4f9058b074482f2c0
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-12-09 10:36:31 -0300

    chore: add eslint and pnpm setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { sum } from './sum';
-console.log("Hello, Spot Events!");
-console.log("Sum of 3 and 5 is:", sum(3, 5));
-
-const arr: string[] = ["Liverpool", "Manchester", "London"];
+const name = 'MyApp';
