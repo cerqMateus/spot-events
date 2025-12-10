b2dde7ab29b2bb5738ab2b0236977704427a5d0b
Mateus Cerqueira <mateus.cerq7@outlook.com>
2025-12-10 09:25:07 -0300

    feat: add express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const name = 'MyApp';
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
